bd62c4dcad1dbe186c88524002c6c27c13997db0
Per Weijnitz <per.weijnitz@gmail.com>
2019-09-17 15:06:50 +0200

    Fetch auth. Adjust params to find-concepts.

--- test/clj/jobtech_taxonomy_api/test/concepts_test.clj
+++ test/clj/jobtech_taxonomy_api/test/concepts_test.clj
@@ -13,7 +13,7 @@
     (concept/assert-concept "skill" "cyklade" "cykla")
     (let [[status body] (util/send-request-to-json-service
                           :get "/v0/taxonomy/public/concepts"
-                          :headers [util/header-auth-user]
+                          :headers [(util/header-auth-user)]
                           :query-params [{:key "type", :val "skill"}])
-          found-concept (first (concept/find-concepts nil "cykla" nil nil nil nil nil))]
+          found-concept (first (concept/find-concepts nil "cykla" nil nil nil nil nil nil nil))]
       (test/is (= "cykla" (get found-concept :preferredLabel))))))
