7d4e769d1f50ddca546f760bf16d1a2e0434cb88
Per Weijnitz <per.weijnitz@gmail.com>
2019-10-24 09:13:26 +0200

    Make relations work in two directions.

--- src/clj/jobtech_taxonomy_api/db/concepts.clj
+++ src/clj/jobtech_taxonomy_api/db/concepts.clj
@@ -62,26 +62,36 @@
 
 (defn handle-relations [query relation related-ids]
 
-  (if (not= "narrower" relation)
+  (if (= "related" relation)
     (-> query
         (update :in conj '?relation '[?related-ids ...])
         (update :args conj relation related-ids)
-        (update :where conj
-                '[?cr :concept/id ?related-ids]
-                '[?r :relation/concept-1 ?cr]
-                '[?r :relation/concept-2 ?c]
-                '[?r :relation/type ?relation])
+        (update :where conj  '[?cr :concept/id ?related-ids]
+                             '[or [and [?r :relation/concept-1 ?cr] [?r :relation/concept-2 ?c]]
+                                  [and [?r :relation/concept-1 ?c] [?r :relation/concept-2 ?cr]]]
+                             '[?r :relation/type ?relation])
         )
+    (if (not= "narrower" relation)
+      (-> query
+          (update :in conj '?relation '[?related-ids ...])
+          (update :args conj relation related-ids)
+          (update :where conj
+                  '[?cr :concept/id ?related-ids]
+                  '[?r :relation/concept-1 ?cr]
+                  '[?r :relation/concept-2 ?c]
+                  '[?r :relation/type ?relation])
+          )
 
-    (-> query
-        (update :in conj '?relation '[?related-ids ...])
-        (update :args conj "broader" related-ids)
-        (update :where conj
-                '[?cr :concept/id ?related-ids]
-                '[?r :relation/concept-1 ?c]
-                '[?r :relation/concept-2 ?cr]
-                '[?r :relation/type ?relation])
-        )
+      (-> query
+          (update :in conj '?relation '[?related-ids ...])
+          (update :args conj "broader" related-ids)
+          (update :where conj
+                  '[?cr :concept/id ?related-ids]
+                  '[?r :relation/concept-1 ?c]
+                  '[?r :relation/concept-2 ?cr]
+                  '[?r :relation/type ?relation])
+          )
+      )
     )
   )
 
